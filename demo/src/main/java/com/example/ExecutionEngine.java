c2e4634f645087ff659c03b9f311fc69ed9518a7
= <=>
2025-03-11 22:27:12 +0530

    updated ImageConvert

--- demo/src/main/java/com/example/ExecutionEngine.java
+++ demo/src/main/java/com/example/ExecutionEngine.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.example.convertion.ImageToGIFFormatConverter;
+import com.example.convertion.URLToImageConverterService;
 import com.example.utils.VideoValidator;
 
 import java.io.IOException;
@@ -27,18 +28,7 @@ public class ExecutionEngine extends Thread{
         int foregroundColor = 000; // Blue
         int backgroundColor = 0xFFFFFFFF; // White
 
-        try {
-            VideoValidator videoValidator = new VideoValidator();
-            if (videoValidator.isValid(inputFile + "tiny.mp4")){
-                ImageToGIFFormatConverter.convertMp4ToGif(inputFile  + "tiny.mp4",
-                        outFile + "mp4.gif",
-                        20);
-            }else {
-                System.out.println("Video does not meet the conditions.");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
 
         System.out.println("Generation Completed");
     }
